f1b197556cba13d50c6f5cde013e7c53dbd3b3a6
chempik1234 <dandijar@yandex.ru>
2021-11-17 20:01:49 +0500

    .ui was replaces with .py random color instead of yellow

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 from random import randint
-from PyQt5 import uic
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from ui_file import Ui_Form
 
 
-class MyWidget(QWidget):
+class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.do_paint = False
-        uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.draw)
 
     def paintEvent(self, event):
@@ -22,8 +22,8 @@ class MyWidget(QWidget):
             qp.end()
 
     def draw_circles(self, qp):
-        qp.setPen(QColor(235, 235, 0))
         for i in range(randint(2, 15)):
+            qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             x, y, r = randint(0, 350), randint(0, 200), randint(30, 50)
             qp.drawEllipse(x, y, r, r)
 
